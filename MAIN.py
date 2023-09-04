4cd80de6862193e3dfd63b58b62cd4e7875c85e4
frandergomez <98371312+frandergomez@users.noreply.github.com>
2023-09-04 11:46:22 -0600

    tablero

--- MAIN.py
+++ MAIN.py
@@ -1,26 +1,78 @@
-def crear_tablero():
-    print("Opción 1: Crear tablero seleccionada")
-    # Agrega aquí tu lógica para crear el tablero
+import os
+import graphviz
 
-def mostrar_datos_estudiante():
-    print("Opción 2: Mostrar datos del estudiante seleccionada")
-    # Agrega aquí tu lógica para mostrar los datos del estudiante
+def crear_tablero(n, m):
+    tablero = [[' ' for _ in range(m)] for _ in range(n)]
+    return tablero
 
-while True:
-    print("*********  Menú  **********")
-    print("1. Crear tablero")
-    print("2. Mostrar datos del estudiante")
-    print("3. Salir")
-    print("***************************")
+def mostrar_tablero(tablero):
+    for fila in tablero:
+        print(' '.join(fila))
 
-    opcion = input("Selecciona una opción (1/2/3): ")
+def colocar_pieza(tablero, fila, columna, color):
+    tablero[fila][columna] = color[0]
+    mostrar_tablero(tablero)
 
-    if opcion == "1":
-        crear_tablero()
-    elif opcion == "2":
-        mostrar_datos_estudiante()
-    elif opcion == "3":
-        print("Saliendo del programa. ¡Hasta luego!")
-        break
-    else:
-        print("Opción no válida. Por favor, selecciona una opción válida (1/2/3).")
+def generar_diagrama(tablero):
+    dot = graphviz.Digraph(format='png')
+    n = len(tablero)
+    m = len(tablero[0])
+    
+    for i in range(n):
+        for j in range(m):
+            if tablero[i][j] != ' ':
+                dot.node(f'{i}_{j}', label=tablero[i][j])
+    
+    for i in range(n):
+        for j in range(m):
+            if i < n - 1:
+                dot.edge(f'{i}_{j}', f'{i+1}_{j}')
+            if j < m - 1:
+                dot.edge(f'{i}_{j}', f'{i}_{j+1}')
+    
+    dot.render('tablero', view=True)
+
+def main():
+    print("Bienvenido al programa de creación de tablero y colocación de piezas.")
+    
+    while True:
+        print("Menu:")
+        print("a. Crear tablero")
+        print("b. Mostrar datos del estudiante")
+        print("c. Salir")
+        
+        opcion = input("Seleccione una opción (a/b/c): ")
+        
+        if opcion == 'a':
+            n = int(input("Ingrese el ancho del tablero: "))
+            m = int(input("Ingrese el largo del tablero: "))
+            tablero = crear_tablero(n, m)
+            
+            while True:
+                mostrar_tablero(tablero)
+                color = input("Seleccione un color (A/R/V/P/N) o 's' para salir: ").upper()
+                
+                if color == 'S':
+                    break
+                
+                fila = int(input("Ingrese la fila donde desea colocar la pieza: "))
+                columna = int(input("Ingrese la columna donde desea colocar la pieza: "))
+                
+                if 0 <= fila < n and 0 <= columna < m:
+                    colocar_pieza(tablero, fila, columna, color)
+                else:
+                    print("Posición inválida. Inténtelo de nuevo.")
+            
+            generar_diagrama(tablero)
+        
+        elif opcion == 'b':
+            print("Carnet del estudiante: 201901371")
+            print("Nombre completo del estudiante: Frander Oveldo Carreto Gómez")
+            print("Nombre del curso y sección: IntroducciÓn a la programación 2 D")
+        
+        elif opcion == 'c':
+            print("Saliendo del programa. ¡Hasta luego!")
+            break
+
+if __name__ == "__main__":
+    main()
